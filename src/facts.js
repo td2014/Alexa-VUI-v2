2af459a260667e2ceafcef289f5ead63ff447afd
td2014 <anthonyd2004@gmail.com>
2017-07-06 07:08:57 -0700

    Added key files

---
+++ src/facts.js
@@ -0,0 +1,22 @@
+// TODO Add more or replace entirely the facts below for your chosen history domain
+// Include at least 10 distinct facts with a 4-digit year included in the text for each
+// Note:  All facts have been obtained from Wikipedia and in some cases, verbatim.
+module.exports = {
+    "FACTS_EN": [
+        "In his 1770 paper, Thoughts On The Algebraic Solution of Equations, Joseph Louis Lagrange studied permutations, one of the elements of abstract algebra.",
+        "Evariste Galois considered, for the first time, the closure properties of a group of permutations in 1832.",
+        "In 1854, Bernhard Riemann presented the first rigorous definition of the integral of a function on an interval to the faculty at the University of Gottingen.",
+        "Gabriel Cramer devised Cramer's Rule, for solving linear systems of equations, in 1750.",
+        "In 1844, Hermann Grassmann published foundational new topics of what is today called linear algebra.",
+        "It was in 1844, that Hermann Grassmann published foundational new topics of what is today called linear algebra.",
+        "Hermann Grassmann, in 1844, published foundational new topics of what is today called linear algebra.",
+        "The 1736 paper by Leonhard Euler, on the Seven Bridges of Konigsberg, is regarded as one of the first practical applications of topology.",
+        "Christiaan Huygens published a book on the subject of probability in 1657.",
+        "In 1933, Andrey Nikolaevich Kolmogorov combined the notion of sample space and measure theory, and presented his axiom system for probability theory.",
+        "The publication of Natural and Political Observations upon the Bills of Mortality, by John Graunt in 1663, is considered by some scholars as the origin of statistics.",
+        "Harvard awarded mathematician Norbert Wiener a Ph.D in 1912, when Wiener was only 17 years old.  He entered Tufts College at the age of 11.",
+        "Lie Groups, named after Sophus Lie, first appeared in French in 1893 in the thesis of Lie's student, Arthur Tresse.",
+        "Emmy Noether's groundbreaking work in abstract algebra began in 1920.",
+        "John Von Neumann made foundational contributions to ergodic theory, in a series of articles published in 1932."
+    ]
+}
---
+++ src/index.js
@@ -0,0 +1,149 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+var APP_ID = undefined;  // can be replaced with your app ID if publishing
+var facts = require('./facts');
+var GET_FACT_MSG_EN = [
+    "Here's your fact: ",
+    "Here you go: ",
+    "One fact coming up: ",
+    "Okay, I found this: ",
+    "Here's one: ",
+    "I don't have anything for that year, but here's a fact you might find interesting: "
+]
+// Test hooks - do not remove!
+exports.GetFactMsg = GET_FACT_MSG_EN;
+var APP_ID_TEST = "mochatest";  // used for mocha tests to prevent warning
+// end Test hooks
+/*
+    TODO (Part 2) add messages needed for the additional intent
+    TODO (Part 3) add reprompt messages as needed
+*/
+var languageStrings = {
+    "en": {
+        "translation": {
+            "FACTS": facts.FACTS_EN,
+            "SKILL_NAME": "My History Facts",  // OPTIONAL change this to a more descriptive name
+            "GET_FACT_MESSAGE": GET_FACT_MSG_EN.slice(0,5), // Five intro messages to select from
+            "GET_FACT_MESSAGE_YEAR_NOT_FOUND_RANDOM": GET_FACT_MSG_EN[5],  // When the year is not found
+            "HELP_MESSAGE": "You can say tell me a fact, or, you can say exit... What can I help you with?",
+            "HELP_REPROMPT": "What can I help you with?",
+            "HELP_REPROMPT_FACTS": "To make a request, say give me a fact, or, give me a fact for 1932.",
+            "STOP_MESSAGE": "Goodbye!"
+        }
+    }
+};
+
+exports.handler = function (event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // set a test appId if running the mocha local tests
+    if (event.session.application.applicationId == "mochatest") {
+        alexa.appId = APP_ID_TEST
+    }
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+/*
+    TODO (Part 2) add an intent for specifying a fact by year named 'GetNewYearFactIntent'
+    TODO (Part 2) provide a function for the new intent named 'GetYearFact' 
+        that emits a randomized fact that includes the year requested by the user
+        - if such a fact is not available, tell the user this and provide an alternative fact.
+    TODO (Part 3) Keep the session open by providing the fact with :askWithCard instead of :tellWithCard
+        - make sure the user knows that they need to respond
+        - provide a reprompt that lets the user know how they can respond
+    TODO (Part 3) Provide a randomized response for the GET_FACT_MESSAGE
+        - add message to the array GET_FACT_MSG_EN
+        - randomize this starting portion of the response for conversational variety
+*/
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'GetFact': function () {
+        // Get a random fact from the facts list
+        // Use this.t() to get corresponding language data
+        var factArr = this.t('FACTS');
+        var randomFact = randomPhrase(factArr);
+
+        // Create speech output
+        // Obtain list of fact prefix messages and randomly select one
+        var prefixArr = this.t("GET_FACT_MESSAGE");
+        var selectedPrefix = randomPhrase(prefixArr);
+        var speechOutput = selectedPrefix + randomFact;
+        var reprompt = this.t("HELP_REPROMPT_FACTS");
+        this.emit(':askWithCard', speechOutput, reprompt, this.t("SKILL_NAME"), randomFact)
+    },
+    'GetNewYearFactIntent': function () {
+        //TODO your code here
+        //
+        // Load fact array:
+        var factArr = this.t('FACTS');
+        //
+        // Get requested year from user input
+
+        var requestedYear = this.event.request.intent.slots.FACT_YEAR.value;
+
+        // Loop over factArr and create new array with entries from the requested year, if any.
+        var yearBasedFactArr = extractYearBasedFacts(factArr, requestedYear);
+
+        // if length of yearBasedFactArr >0, select a random fact from the array containing facts from that year only.
+        if (yearBasedFactArr.length > 0) {
+           // Choose a random fact from those that contain the requested year.
+           var yearBasedFact = randomPhrase(yearBasedFactArr); 
+           // Obtain list of fact prefix messages and randomly select one
+           var prefixArr = this.t("GET_FACT_MESSAGE");
+           var selectedPrefix = randomPhrase(prefixArr);
+           var speechOutput = selectedPrefix + yearBasedFact;
+           var reprompt = this.t("HELP_REPROMPT_FACTS");
+           this.emit(':askWithCard', speechOutput, reprompt, this.t("SKILL_NAME"), yearBasedFact);
+        } else { 
+           // otherwise load random fact
+           var randomFact = randomPhrase(factArr);
+           // Create speech output corresponding to this
+           var speechOutput = this.t("GET_FACT_MESSAGE_YEAR_NOT_FOUND_RANDOM") + randomFact;
+           var reprompt = this.t("HELP_REPROMPT_FACTS");
+           this.emit(':askWithCard', speechOutput, reprompt, this.t("SKILL_NAME"), randomFact);
+        }
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = this.t("HELP_MESSAGE");
+        var reprompt = this.t("HELP_MESSAGE");
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    }
+};
+
+function randomPhrase(phraseArr) {
+    // returns a random phrase
+    // where phraseArr is an array of string phrases
+    var i = 0;
+    i = Math.floor(Math.random() * phraseArr.length);
+    return (phraseArr[i]);
+};
+
+// Function to return array with facts based on a particular year, if they exist
+function extractYearBasedFacts(factArr, requestedYear) {
+    // Initialize return array
+    var yearBasedFactArr = [];
+    // scan over each fact, looking for matches with the requested year.  If one exists, add to return array
+    var iFact;
+    for (iFact = 0; iFact < factArr.length ; iFact++) {
+        var currFact = factArr[iFact];
+        if (currFact.indexOf(requestedYear) >= 0) {
+            yearBasedFactArr.push(factArr[iFact]);
+        }
+    }
+    return (yearBasedFactArr);
+};
